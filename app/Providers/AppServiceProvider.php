603029937fc4c31dba993854082f2b7e76e7c2e9
wintreetech <shubhammakwana@wintreetech.com>
2026-03-20 17:26:41 +0530

    temp

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,8 +14,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-       if (app()->environment('production')) {
             URL::forceScheme('https');
-        }
     }
 }
